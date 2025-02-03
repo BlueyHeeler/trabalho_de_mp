4260312b6acff476c51cfa91a4c1d7ee870fe6d7
BlueyHeeler <tairaerick@gmail.com>
2025-02-03 10:55:43 -0300

    Testando como arrumar o HTML da página de login.

--- feira/app/helpers/shoppings_helper.rb
+++ feira/app/helpers/shoppings_helper.rb
@@ -1,5 +1,2 @@
-module ShoppingHelpers
-    def current_user
-      @current_user ||= User.first
-    end
+module ShoppingsHelpers
 end
--- feira/app/models/concerns/distance_calculator.rb
+++ feira/app/models/concerns/distance_calculator.rb
@@ -4,12 +4,15 @@ module DistanceCalculator
   EARTH_RADIUS = 6371 # km
   RAD_PER_DEG = Math::PI/180
 
+  # Struct to hold coordinates in radians
   Coordinates = Struct.new(:lat_rad, :lon_rad) do
+    # Converts latitude and longitude from degrees to radians
     def self.from_degrees(lat, lon)
       new(lat * RAD_PER_DEG, lon * RAD_PER_DEG)
     end
   end
 
+  # Calculates the Haversine distance between two sets of coordinates
   def self.haversine_distance(origin_coords, destination_coords)
     origin = convert_to_radians(origin_coords)
     destination = convert_to_radians(destination_coords)
@@ -19,11 +22,13 @@ module DistanceCalculator
 
   private
 
+  # Converts coordinates from degrees to radians
   def self.convert_to_radians(coords)
     lat, lon = coords
     Coordinates.from_degrees(lat, lon)
   end
 
+  # Calculates the differences in latitude and longitude
   def self.calculate_deltas(origin, destination)
     Coordinates.new(
       destination.lat_rad - origin.lat_rad,
@@ -31,11 +36,13 @@ module DistanceCalculator
     )
   end
 
+  # Calculates the final distance using the Haversine formula
   def self.calculate_final_distance(origin, destination, delta)
     haversine_half = Math.sin(delta.lat_rad/2)**2 +
                      Math.cos(destination.lat_rad) *
                      Math.cos(origin.lat_rad) *
                      Math.sin(delta.lon_rad/2)**2
+    
     central_angle = 2 * Math.atan2(Math.sqrt(haversine_half), Math.sqrt(1-haversine_half))
     EARTH_RADIUS * central_angle
   end
